94981314ed651e95ad801b19cec8746a4b9db2ae
Mikko Ala-Fossi <mikko.alafossi@gmail.com>
2019-03-21 21:19:59 +0200

    initial commit

---
+++ app/Ananas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+class Ananas
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+}
