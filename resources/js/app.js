5d8b4aa6b3a508c5cf278c0029a2c3f960ffb726
Mikko Ala-Fossi <mikko.alafossi@gmail.com>
2019-03-26 21:48:29 +0200

    Make VUE load in the header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,29 @@
+require('./bootstrap');
+window.Vue = require('vue');
+
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
 import Vuex from 'vuex';
-import {routes} from './routes';
+//import {routes} from './routes';
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(VueAxios, axios);
 
 import MainApp from './components/App';
-
+Vue.component('main-app', require('./components/MainApp.vue'));
 
 const router = new VueRouter({
     mode: 'history',
     router
 });
-
+const app = new Vue(Vue.util.extend({ router })).$mount('#app');
+/*
 const app = new Vue({
     el: '#app',
     components: { App },
     router,
 });
+*/
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vue SPA Demo</title>
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     <div id="app">
         <main-app></main-app>
     </div>
 
-    <script src="{{ assets('js/app.js') }}"></script>
+    
 </body>
 </html>
