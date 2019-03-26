adb51978a0ba1ca4b4a0ee99ef2979e2b284c87e
Mikko Ala-Fossi <mikko.alafossi@gmail.com>
2019-03-26 21:12:32 +0200

    Add Laravel\Passport as authentication service
    https://medium.com/techcompose/create-rest-api-in-laravel-with-authentication-using-passport-133a1678a876

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,12 @@ Route::prefix('v2')->group(function () {
         return 'hi';
     });
 
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
+    
+    Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'UserController@details');
+
 });
 
 // Legacy API
