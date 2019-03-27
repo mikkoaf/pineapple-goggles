892dcaa7774d287e43d8700f4ab6e68498a0c057
Mikko Ala-Fossi <mikko.alafossi@gmail.com>
2019-03-27 20:00:39 +0200

    Add basic location history relations

--- app/LocationHistory.php
+++ app/LocationHistory.php
@@ -16,4 +16,15 @@ class LocationHistory extends Model
         'latitude',
         'longitude',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'foreign_key', 'other_key');
+    }
+
+    public function dialoguePerson()
+    {
+        return $this->belongsTo('App\DialoguePerson');
+    }
+
 }
