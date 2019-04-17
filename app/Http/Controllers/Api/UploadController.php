0e2f3b533dc52383c76fbfa3418a8eabdc9d48d3
Mikko Ala-Fossi <mikko.alafossi@gmail.com>
2019-04-17 20:58:34 +0300

    Document singular file uploading
    Files can be uploaded as n array or one at a time. Let's begin with
    single files.

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -10,17 +10,42 @@ use App\Http\Controllers\Controller;
 class UploadController extends Controller
 {
     /**
+     * Returns text messages of a DialoguePerson
+     * @OA\Post(
+     *      path="/api/upload",
+     *      tags={"File upload"},
+     *      summary="Upload files",
+     *     @OA\Parameter(
+     *         name="upload",
+     *         in="body",
+     *         description="Limit the number of items per query",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="file"
+     *         )
+     *     ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     *       @OA\Response(response=400, description="Bad request"),
+     *
+     *    )
      *
      * https://stackoverflow.com/questions/46633582/laravel-file-upload-api-using-postman
+     *
+     * https://laravel.com/docs/5.8/filesystem
+     *
+     * stores files uploaded correctly to storage/app/public
      *
      * @param Request $request
      */
     public function upload(Request $request)
     {
-        $files = $request->file('upload');
-        if(!empty($files)) {
-            foreach($files as $file) {
-                Storage::put($file->getClientOriginalName(),file_get_contents($file));
-            }
+        $file = $request->file('upload');
+        if (!empty($file)) {
+            Storage::put($file->getClientOriginalName(), file_get_contents($file));
         }
+    }
 }
